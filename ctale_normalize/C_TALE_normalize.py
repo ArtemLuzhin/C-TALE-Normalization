1a3f337914504c3510736ba4b7bf4e58dfc2d45d
Phlya <flyamer@gmail.com>
2019-10-17 18:18:32 +0100

    First try to use sparse etc but include zone 3 in IC

--- ctale_normalize/C_TALE_normalize.py
+++ ctale_normalize/C_TALE_normalize.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import cooler
 import scipy.stats
-from scipy import sparse
+from scipy import sparse, stats
 import sys
 from natsort import natsorted
 
@@ -39,35 +39,6 @@ def CTALE_norm_multiplicate(mtx, ROI_start, ROI_end, resolution,
     normalized[i_lower] = normalized.T[i_lower]
     return(normalized)
 
-def norm_mtx(mtx, func=scipy.stats.gmean):
-    normalized = np.zeros_like(mtx)
-    for i in range(mtx.shape[0]):
-        normalized[i,i:]=mtx[i,i:]/(np.sum(mtx[i,i:])+np.sum(mtx[:i,i])) #left
-        normalized[:i,i]=mtx[:i,i]/(np.sum(mtx[:i,i])+np.sum(mtx[i,i:])) #up
-    for i in range(mtx.shape[0]):
-        for j in range(i+1, mtx.shape[0]):
-            normalized[i,j]=mtx[i,j]/func([np.sum(mtx[i,:]),np.sum(mtx[:,j])])
-    i_lower = np.tril_indices(normalized.shape[0], -1) #creates symmetric matrix
-    normalized[i_lower] = normalized.T[i_lower]
-    return normalized
-
-def CTALE_norm(mtx, ROI_start, ROI_end, resolution, func=scipy.stats.gmean):
-    """Same as CTALE_norm_multiplicate function but without multiplication.
-    mtx- matrix of individual chromosome/region +/- distance
-    ROI_start - first coordinate of C-TALE region(bp)
-    ROI_end - last coordinate of C-TALE region(bp)
-    resolution - C-TALE map resolution(bp)
-    func - function for calculating mean, can be numpy.mean,numpy.median and etc. Default scipy.stats.gmean
-    returns normalized matrix"""
-    start_bin = ROI_start//resolution
-    end_bin = ROI_end//resolution
-    #fill main diagonal with zeros
-    mtx.setdiag(0)
-    submtx = mtx[start_bin:end_bin+1, start_bin:end_bin+1].todense()
-    normed = norm_mtx(submtx)
-    mtx[start_bin:end_bin+1, start_bin:end_bin+1] = sparse.csr_matrix(normed)
-    return mtx
-
 def multiplicate(mtx, ROI_start, ROI_end, resolution, mult=1.54):
     """mtx- matrix of individual chromosome/region +/- distance
     ROI_start - first coordinate of C-TALE region(bp)
@@ -85,9 +56,37 @@ def multiplicate(mtx, ROI_start, ROI_end, resolution, mult=1.54):
                                                             start_bin:end_bin+1]
     return new_mtx
 
-def CTALE_norm_iterative(mtx, ROI_start, ROI_end, resolution,
-                         func=scipy.stats.gmean, mult=1.54, steps=20,
-                         tolerance=1e-5):
+def get_cov_var(mtx, startbin, endbin):
+    cov1 = np.asarray(mtx.sum(axis=0)).ravel()
+    cov2 = np.asarray(mtx.sum(axis=1)).ravel()
+    
+    cov1[:startbin] = 1
+    cov1[endbin+1:] = 1
+    cov1[endbin+1:] = 1
+    
+    cov2[:startbin] = 1
+    cov2[endbin+1:] = 1
+    
+    cov = stats.gmean([cov1, cov2], axis=0)
+    var = np.std(cov[startbin:endbin+1])
+    
+    return cov1, cov2, var
+
+def CTALE_norm(mtx, ROI_start, ROI_end, resolution):
+    """Single iteration of CTALE balancing
+    mtx - matrix of individual chromosome/region +/- distance
+    ROI_start - first coordinate of C-TALE region(bp)
+    ROI_end - last coordinate of C-TALE region(bp)
+    resolution - C-TALE map resolution(bp)
+    returns normalized matrix and variance at this iteration"""
+    start_bin = ROI_start//resolution
+    end_bin = ROI_end//resolution
+    cov1, cov2, var = get_cov_var(mtx, start_bin, end_bin)
+    mtx = mtx.multiply(1/cov1).multiply(1/cov2[np.newaxis].T).tocsr()
+    return mtx, var
+
+def CTALE_norm_iterative(mtx, ROI_start, ROI_end, resolution, mult=1.54,
+                         target_var=10**-6, steps=20):
     """Main function that perform normalization until variance>tolerance
     mtx- matrix of individual chromosome/region +/- distance
     ROI_start - first coordinate of C-TALE region(bp)
@@ -100,20 +99,23 @@ def CTALE_norm_iterative(mtx, ROI_start, ROI_end, resolution,
     returns normalized matrix"""
     start_bin = ROI_start//resolution
     end_bin = ROI_end//resolution
+    
     out = multiplicate(mtx=mtx, ROI_start=ROI_start, ROI_end=ROI_end,
-                       resolution=resolution, mult=mult)
-    for s in range(steps):
-        out = CTALE_norm(out, ROI_start, ROI_end, resolution, func=func)
-        var = np.var(np.sum(out[start_bin:end_bin+1, start_bin:end_bin+1],
-                            axis=1))
-        print('Variance is: ',var)
-        if var<tolerance:
-            print('Variance < ',tolerance)
-            break
-    factor = out[start_bin:end_bin+1, start_bin:end_bin+1].sum().sum()/mtx[start_bin:end_bin+1, start_bin:end_bin+1].sum().sum()
-    out *= factor
-    out[start_bin:end_bin+1, start_bin:end_bin+1] /= factor
-    return out
+                   resolution=resolution, mult=mult)
+    
+    cov1, cov2, var = get_cov_var(out, start_bin, end_bin)
+    print('Original var:', var)
+    for i in range(steps):
+        out, var = CTALE_norm(out, ROI_start, ROI_end, resolution)
+        print('Iteration %s: var: %s' % (i+1, var))
+        if var < target_var:
+            print('Variance below %s' % target_var)
+#            factor = out[start_bin:end_bin+1, start_bin:end_bin+1].sum()/mtx[start_bin:end_bin+1, start_bin:end_bin+1].sum()
+#            out *= factor
+#            out[start_bin:end_bin+1, start_bin:end_bin+1] /= factor
+            return out
+    raise ValueError('Too many interation without convergence')
+
 
 def get_pixels(mtx, zero_id=0):
     mtx_upper_diag = sparse.triu(mtx, k=0)
